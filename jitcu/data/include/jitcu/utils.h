0e9353e120b13ed565a16e57950e6f74a9223a0e
monellz <rxzhong@outlook.com>
2025-08-20 13:36:46 +0800

    Fix typo

--- jitcu/data/include/jitcu/utils.h
+++ jitcu/data/include/jitcu/utils.h
@@ -31,8 +31,14 @@ _JITCU_DEVICE void dump_rowmajor_matrix(T* addr, int rows, int cols) {
   printf("\n");
 }
 
+// following functions needs cutlass/cute
+#if defined(CUTE_HOST_DEVICE)
+
 template <typename T>
-_JITCU_DEVICE void dump_cute_matrix(const T& addr, int rows, int cols) {
+_JITCU_DEVICE void dump_cute_matrix(const T& addr) {
+  static_assert(decltype(cute::rank(addr))::value == 2, "only support 2D tensor");
+  int rows = decltype(cute::size<0>(addr))::value;
+  int cols = decltype(cute::size<1>(addr))::value;
   printf("Dumping %d x %d cute matrix\n", rows, cols);
   for (int i = 0; i < rows; ++i) {
     for (int j = 0; j < cols; ++j) {
@@ -42,6 +48,37 @@ _JITCU_DEVICE void dump_cute_matrix(const T& addr, int rows, int cols) {
   }
   printf("\n");
 }
+
+template <typename T>
+_JITCU_DEVICE void dump_cute_vector(const T& addr) {
+  static_assert(decltype(cute::rank(addr))::value == 1, "only support 1D tensor");
+  int n = decltype(cute::size<0>(addr))::value;
+  printf("Dumping %d size cute vector\n", n);
+  for (int i = 0; i < n; ++i) {
+    printf("%8.4f ", double(addr(i)));
+  }
+  printf("\n");
+}
+
+template <bool ColumnMajor, int BLOCK_M, int BLOCK_N, typename TiledMma, typename TensorC>
+__forceinline__ __device__ void store_Cregs_into_smem(TiledMma& tiled_mma, TensorC& acc,
+                                                      typename TensorC::value_type* smem_ptr) {
+  using DType = typename TensorC::value_type;
+  using SmemCopyAtom = cute::Copy_Atom<cute::UniversalCopy<DType>, DType>;
+  auto smem_tiled_copy = cute::make_tiled_copy_C(SmemCopyAtom{}, tiled_mma);
+  auto smem_thr_copy = smem_tiled_copy.get_thread_slice(threadIdx.x);
+  using SmemLayout = std::conditional_t<
+      ColumnMajor,
+      decltype(cute::make_layout(cute::make_shape(cute::Int<BLOCK_M>{}, cute::Int<BLOCK_N>{}),
+                                 cute::GenColMajor{})),
+      decltype(cute::make_layout(cute::make_shape(cute::Int<BLOCK_M>{}, cute::Int<BLOCK_N>{}),
+                                 cute::GenRowMajor{}))>;
+  cute::Tensor smem = cute::make_tensor(cute::make_smem_ptr(smem_ptr), SmemLayout{});
+  cute::Tensor tOs = smem_thr_copy.partition_D(cute::as_position_independent_swizzle_tensor(smem));
+  cute::copy(smem_tiled_copy, smem_thr_copy.retile_S(acc), tOs);
+}
+
+#endif
 
 }  // namespace jc::utils
 
